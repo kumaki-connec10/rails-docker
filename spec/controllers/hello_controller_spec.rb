759362186b1fce91a30485cb0d1671ed51426741
Mamoru Kumaki <mamoru.kumaki@rakuten-lifullstay.com>
2018-07-31 15:38:57 +0900

    circleci

--- spec/controllers/hello_controller_spec.rb
+++ spec/controllers/hello_controller_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe HelloController, type: :controller do
+  let(:result) { 1 }
+  context "test" do
+    it "hoge" do
+      expect(result).to eq 1
+    end
+  end
 
 end
